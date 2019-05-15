a7dd5f9ca292e4b59806c29fdf8e63b51662f1cc
EssenceOfChaos <fjschiller@gmail.com>
2019-05-15 12:37:58 -0400

    fix failing test

--- test/xxo_test.exs
+++ test/xxo_test.exs
@@ -1,8 +1,12 @@
 defmodule XxoTest do
+  @moduledoc """
+  Testing the Xxo module
+  """
   use ExUnit.Case
   doctest Xxo
 
   test "greets the world" do
-    assert Xxo.hello() == :world
+    id = Xxo.generate_game_id()
+    assert is_binary(id)
   end
 end
